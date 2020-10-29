7e331761158851194a08a1011063742a20e14527
Vinicius Silva <vinicius.namiuchi@hotmail.com>
2020-10-29 17:47:44 -0300

    Criado arquivo bola.js

--- animacao.js
+++ animacao.js
@@ -1,12 +1,14 @@
-function Animacao() {
-    this.sprite = [];
+function Animacao(context) {
+    this.context = context;
+    this.sprites = [];
     this.ligado = false;
 }
 
 Animacao.prototype = {
     novoSprite: function(sprite) {
-        this.sprite.push(sprite);
+        this.sprites.push(sprite);
     },
+
     ligar: function() {
         this.ligado = true;
         this.proximoFrame();
@@ -14,5 +16,27 @@ Animacao.prototype = {
 
     desligar: function() {
         this.ligado = false;
+    },
+
+    proximoFrame: function() {
+        if (! this.ligado) return;
+
+        this.limparTela();
+
+        for (var i in this.sprites)
+            this.sprites[i].atualizar();
+
+        for (var i in this.sprites)
+            this.sprites[i].desenhar();
+
+        var animacao = this;
+        requestAnimationFrame(function () {
+            animacao.proximoFrame();
+        });
+    },
+
+    limparTela: function() {
+        var ctx = this.context;
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     }
 }
